488494f01e95c453a4b19a07bed392878572f8a5
mjwatson96 <matthewwatson96@gmail.com>
2015-10-11 17:45:21 +1100

    hopefully finished hashmap.java

--- HashMap.java
+++ HashMap.java
@@ -56,7 +56,121 @@ public class HashMap<K extends Comparable<K>, V> {
         }
         return keys;
     }
-    public V put(K key, V value)
-    public V get(K key)
-    public V remove(K key)
+
+    public V put(K key, V value){
+        int index = hash(key) % hashMapSize;
+        int count = index;
+
+        if(items[index] == null){
+            items[index] = HashMapNode<>(key, value);
+            return null;
+        }
+
+        if(items[count].getKey().equals(key)){
+            V original = items[count].getValue();
+            items[count].setValue(value);
+            return original;
+        }
+
+        else if(!items[index].getKey().equals(key)){
+            Collisions++;
+            for(int i=0; i<hashMapSize; i++){
+                count++;
+                totalCollisions++;
+                if(count>=hashMapSize){
+                    count = 0;
+                }
+
+                if(i>maxCollisions){
+                    maxCollisions=i;
+                }
+
+                if(items[count]==null){
+                    items[count] = new HashMapNode<>(key, value);
+                    return null;
+                }
+
+                if(items[count].getKey().equals(key)){
+                    V original= items[count].getValue();
+                    items[count].setValue(value);
+                    return original;
+                }
+            }
+        }
+        return null;
+    }
+
+    public V get(K key){
+        int index = hash(key) % hashMapSize;
+        HashMapNode<K, V> entry= items[index];
+        int count = index;
+
+        if(entry==null){
+            return null;
+        }
+
+        //do a check to see if the key is the one we're looking for
+        if(!entry.key.equals(key)){
+            for(int i=0; i<hashMapSize: i++){
+                count++;
+                if(count>=hashMapSize){
+                    count=0;
+                }
+                if(items[count].getKey().equals(key)){
+                    return items[count].getValue();
+                }
+            }
+        }
+        return entry.getValue();
+    }
+
+    public V remove(K key){
+        int index = hash(key) % hashMapSize;
+        int count = index;
+        HashMapNode(K,V) entry=items[index];
+
+        if(entry==null){
+            return null;
+        }
+
+        if(entry.key.equals(key)){
+            V tmp = entry.value;
+            items[index] = null;
+            return tmp;
+        }
+
+        if(!entry.key.equals(key)){
+            for(int i=0; i<hashMapSize; i++){
+                count++;
+                if(count>=hashMapSize){
+                    count=0;
+                }
+                if(items[count].getKey().equals(key)){
+                    V tmp=items[count].getValue();
+                    items[count]=null;
+                    return tmp;
+                }
+            }
+        }
+        return null;
+    }
+
+    public int Collisions(){
+        return this.Collisions;
+    }
+
+    public int totalCollisions(){
+        return this.totalCollisions;
+    }
+
+    public int maxCollisions(){
+        return this.maxCollisions;
+    }
+
+    public void resetStatistics(){
+        Collisions=0;
+        totalCollisions=0;
+        maxCollisions=0;
+    }
+
 }
